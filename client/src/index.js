3f2597f665be72492e7ff19d80de7fd83db0ff41
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-12-17 10:11:24 -0600

    styling

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,11 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import {Provider} from 'react-redux'
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 registerServiceWorker();
